a691b39919177181f302a371439f931678eff006
ht3aa <ht3aa2001@gmail.com>
2022-09-22 15:35:21 +0300

    adding remove btn

--- main.js
+++ main.js
@@ -29,11 +29,8 @@ function makeScatterPlotGraph(xValues, yValues) {
     height = window.innerHeight,
     margin = { top: 100, right: 100, bottom: 100, left: 100 };
 
-  const svg = d3
-    .select("#scatterPlotGraph")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  const div = d3.select("#scatterPlotGraph").append("div");
+  const svg = div.append("svg").attr("width", width).attr("height", height);
 
   const xScale = d3
     .scaleLinear()
@@ -94,4 +91,31 @@ function makeScatterPlotGraph(xValues, yValues) {
     .append("g")
     .call(d3.axisBottom(xScale))
     .attr("transform", `translate(0, ${height - margin.bottom})`);
+
+  div
+    .append("button")
+    .attr(
+      "class",
+      "mb-10 ml-20 bg-red-600 py-2 px-5 text-white hover:bg-red-800"
+    )
+    .attr("onclick", "removeChart(this)")
+    .text("Remove chart");
+
+  d3.select("#indication")
+    .attr("class", "mt-3 text-blue-600 font-bold")
+    .text("There are charts above. scroll up to see them");
+}
+
+function removeChart(target) {
+  const divElement = target.parentNode;
+
+  divElement.remove();
+
+  if (d3.select("#scatterPlotGraph").selectChildren()._groups[0].length === 0) {
+    d3.select("#indication")
+      .attr("class", "mt-3 text-blue-600 font-bold")
+      .text("");
+  }
+
+  console.log();
 }
